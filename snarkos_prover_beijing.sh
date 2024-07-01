e15e01fe138ecdfc2cd2a5c7107404f274970748
like <likeaixi@gmail.com>
2024-07-01 15:58:22 +0800

    update shell

--- snarkos_prover_beijing.sh
+++ snarkos_prover_beijing.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup ./snarkos start --prover --metrics --private-key=APrivateKey1zkpEKTcE8WJGDDLh1BzJPhwbnW3mzyBuAmrg7Cz6r8GYPEF --verbosity=2 --network 1 --nodisplay --connect 192.168.0.182:4130 >>./snarkos-prover-`date +%Y-%m-%d`.log  2>&1 &
+nohup ./snarkos start --prover --metrics --private-key="$1" --verbosity=2 --network 1 --nodisplay --connect 192.168.0.182:4130 >>./snarkos-prover-`date +%Y-%m-%d`.log  2>&1 &
