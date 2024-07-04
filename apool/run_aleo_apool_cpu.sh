87485bb77c99d13c0107440f5afea3cc44e6075c
like <likeaixi@gmail.com>
2024-07-04 15:27:50 +0800

    update apool

--- apool/run_aleo_apool_cpu.sh
+++ apool/run_aleo_apool_cpu.sh
@@ -3,6 +3,6 @@ if ps aux | grep 'apoolminer' | grep -q 'apool.io'; then
     echo "ApoolMiner already running."
     exit 1
 else
-    nohup  ./apoolminer --pool aleo1.hk.apool.io:9090 --gpu-off --account CP_32hb86nvxc --worker worker_01 -A aleo >> aleo.log 2>&1 &
+    nohup  ./apoolminer --pool aleo1.hk.apool.io:9090 --gpu-off --account CP_asmku88wik -A aleo >> aleo.log 2>&1 &
 fi
 
