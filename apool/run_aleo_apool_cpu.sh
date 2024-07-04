045a210c3f3931621a548659ff7ac14344842103
like <likeaixi@gmail.com>
2024-07-04 16:09:26 +0800

    update apool

--- apool/run_aleo_apool_cpu.sh
+++ apool/run_aleo_apool_cpu.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function ceil(){
+  floor=`echo "scale=0;$1/1"|bc -l ` # 向下取整
+  add=`awk -v num1=$floor -v num2=$1 'BEGIN{print(num1<num2)?"1":"0"}'`
+  echo `expr $floor  + $add`
+}
+
 max_cpu_processor="cat /proc/cpuinfo| grep "processor"| wc -l"
 
 cpu_processor=`ceil "$max_cpu_processor" \* 80 / 100`
@@ -11,10 +17,3 @@ if ps aux | grep 'apoolminer' | grep -q 'apool.io'; then
 else
     nohup  ./apoolminer --pool aleo1.hk.apool.io:9090 --gpu-off --account CP_asmku88wik -A aleo -t "$cpu_processor" >> aleo.log 2>&1 &
 fi
-
-
-function ceil(){
-  floor=`echo "scale=0;$1/1"|bc -l ` # 向下取整
-  add=`awk -v num1=$floor -v num2=$1 'BEGIN{print(num1<num2)?"1":"0"}'`
-  echo `expr $floor  + $add`
-}
