eadd75625308f039191bd28cef397a4b7c04348a
like <likeaixi@gmail.com>
2024-09-04 17:44:21 +0800

    add oula pool

---
+++ oula/oula_aleo_gpu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 获取第一个非本地网络接口的 IP 地址
+ip_address=$(hostname -I | awk '{print $1}')
+
+# 去掉 IP 地址中的点
+ip_address_no_dots=$(echo $ip_address | tr -d '.')
+
+# 打印处理后的 IP 地址
+echo "IP 地址: $ip_address_no_dots"
+
+
+nohup /root/aleo/oula/oula-pool-prover --pool wss://aleo.oula.network:6666 --account hty2024  --worker-name $ip_address_no_dots >> ./aleo-oula-prover.log 2>&1 &
