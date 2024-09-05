f795771197770bc93ba136ff03ca9c41e686918e
like <likeaixi@gmail.com>
2024-09-05 10:36:12 +0800

    update f2pool watch dog

--- f2pool/f2pool_aleo_cpu.sh
+++ f2pool/f2pool_aleo_cpu.sh
@@ -9,5 +9,5 @@ ip_address_no_dots=$(echo $ip_address | tr -d '.')
 # 打印处理后的 IP 地址
 echo "IP 地址: $ip_address_no_dots"
 
-nohup ./aleo-miner -u stratum+tcp://aleo-asia.f2pool.com:4400 -w hty2021.$ip_address_no_dots >> ./aleo-miner.log 2>&1 &
+nohup /root/aleo/f2pool/aleo-miner -u stratum+tcp://aleo-asia.f2pool.com:4400 -w hty2021.$ip_address_no_dots >> /root/aleo/f2pool/aleo-miner.log 2>&1 &
 
