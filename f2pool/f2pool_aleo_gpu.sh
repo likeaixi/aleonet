1df34c4239fdfefdf73095a51009438c4635dc4c
like <likeaixi@gmail.com>
2024-09-05 10:32:41 +0800

    update f2pool

--- f2pool/f2pool_aleo_gpu.sh
+++ f2pool/f2pool_aleo_gpu.sh
@@ -9,5 +9,5 @@ ip_address_no_dots=$(echo $ip_address | tr -d '.')
 # 打印处理后的 IP 地址
 echo "IP 地址: $ip_address_no_dots"
 
-nohup ./aleo-miner -u stratum+tcp://aleo-asia.f2pool.com:4400 -w hty2021.$ip_address_no_dots -d 0 >> ./aleo-miner.log 2>&1 &
+nohup /root/aleo/f2pool/aleo-miner -u stratum+tcp://aleo-asia.f2pool.com:4400 -w hty2021.$ip_address_no_dots -d 0,1,2,3,4,5,6,7 >> /root/aleo/f2pool/aleo-miner-`date +%Y-%m-%d`.log 2>&1 &
 
